f5648cae4fa18e03fcf4ab7477727cef4734b8f4
Fabio Cortés <fcor14@hotmail.com>
2018-12-09 10:05:40 -0500

    Navbar and topSection ok

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { NavLink } from  'react-router-dom'
+import './styles.css'
+import Logo from '../Logo'
+
+const Navbar = ({variant, type}) => {
+    return(
+      <div className={`navbar row ${variant}`}>
+        <Logo variant="link" type={type} />
+        <div className="menu row">
+          <NavLink
+            to='/semilleros'
+            activeClassName="selected"
+            >
+              Semilleros
+          </NavLink>
+          <NavLink
+            to='/revista'
+            activeClassName="selected"
+            >
+            Revista Reto
+          </NavLink>
+          <NavLink
+            to='/proyectos'
+            activeClassName="selected"
+            >
+            Proyectos
+          </NavLink>
+          <NavLink
+            to='/simposio'
+            activeClassName="selected"
+            >
+            Simposio
+          </NavLink>
+        </div>
+      </div>
+    )
+}
+
+Navbar.defaultProps = {
+  variant: 'classic'
+}
+
+export default Navbar
--- src/components/TopSection/index.js
+++ src/components/TopSection/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Navbar from '../Navbar'
+import './styles.css'
 
-const TopSection = () => {
-    return(
-        <div>TopSection</div>
-    )
+const TopSection = ({ location }) =>{
+    if (location === '/') {
+      return(
+        <div className="top-section img">
+          <div className="container">
+            <Navbar variant="home" type="home" />
+            <div className="top-section-content column">
+
+            </div>
+          </div>
+        </div>
+      )
+    } else{
+        return(
+          <div className="top-section-default">
+            <div className="container">
+              <Navbar />
+            </div>
+          </div>
+        )
+    }
 }
-
 export default TopSection
