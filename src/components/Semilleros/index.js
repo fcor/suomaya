f0fc468a2adb9eafa9e4dfc1f152f5c182e574f3
Fabio Cortés <fcor14@hotmail.com>
2018-12-18 10:21:38 -0500

    Semilleros Header ok

--- src/components/Semilleros/index.js
+++ src/components/Semilleros/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './styles.css'
 
 const Semilleros = () => {
     return(
-        <div>Semilleros</div>
+        <div className="semilleros column">
+          <div className="box"></div>
+          <section className="semilleros-header column">
+            <h1>Semilleros Suomaya</h1>
+            <p>
+              Dragée sweet roll fruitcake candy. Sugar plum pastry caramels 
+              halvah biscuit powder. Jujubes jelly beans oat cake. Pudding 
+              wafer danish wafer danish muffin dragée cookie. Brownie oat cake
+              croissant. Bear claw sweet caramels toffee apple pie gingerbread. 
+              Cake candy canes icing jelly jelly beans dessert dessert. Marzipan
+              candy canes marzipan. 
+            </p>
+          </section>
+        </div>
     )
 }
 
