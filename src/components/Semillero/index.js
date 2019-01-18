82a8e914e9dd659dca8e0485b67b4f7e6fc7ce9c
Fabio Cortés <fcor14@hotmail.com>
2019-01-18 11:57:43 -0500

    Some text and scroll fixes

--- src/components/Semillero/index.js
+++ src/components/Semillero/index.js
@@ -4,32 +4,38 @@ import logistica from '../../assets/img/logistica.svg'
 import TeamMembers from './TeamMembers.js'
 import getSemillero from '../../utils/semilleros'
 
-const Semillero = ({variant}) => {
-  const semillero = getSemillero(variant)
-  return (
-    <div className="semillero-detail column">
-      <section className="semillero-header column">
-        <img src={semillero.logo} alt="logo" />
-        <h1>{semillero.name}</h1>
-        <p>{semillero.description}</p>
-      </section>
-      <section className="semillero-team">
-        <div className="team-content column">
-          <h1>Nuestro Equipo</h1>
-          <TeamMembers members={semillero.team} />
-        </div>
-      </section>
-      <section className="semillero-join column">
-        <h1>¿Quieres ser parte de nuestro grupo?</h1>
-        <p>Cupcake ipsum dolor. Sit amet caramels. Apple pie wafer carrot cake powder gummies 
-          chupa chups jujubes. Topping soufflé fruitcake. Apple pie cake chupa chups icing cupcake
-          sweet roll jelly-o. Bear claw cake tootsie roll jujubes lollipop donut soufflé croissant 
-          wafer.
-        </p>
-        <h2>innovatics@sena.edu.co</h2>
-      </section>
-    </div>
-  )
+class Semillero extends React.Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+  render() {
+    const { variant } = this.props
+    const semillero = getSemillero(variant)
+    return (
+      <div className="semillero-detail column">
+        <section className="semillero-header column">
+          <img src={semillero.logo} alt="logo" />
+          <h1>{semillero.name}</h1>
+          <p>{semillero.description}</p>
+        </section>
+        <section className="semillero-team">
+          <div className="team-content column">
+            <h1>Nuestro Equipo</h1>
+            <TeamMembers members={semillero.team} />
+          </div>
+        </section>
+        <section className="semillero-join column">
+          <h1>¿Quieres ser parte de nuestro grupo?</h1>
+          <p>Cupcake ipsum dolor. Sit amet caramels. Apple pie wafer carrot cake powder gummies 
+            chupa chups jujubes. Topping soufflé fruitcake. Apple pie cake chupa chups icing cupcake
+            sweet roll jelly-o. Bear claw cake tootsie roll jujubes lollipop donut soufflé croissant 
+            wafer.
+          </p>
+          <h2>innovatics@sena.edu.co</h2>
+        </section>
+      </div>
+    )
+  }
 }
 
 export default Semillero
--- src/utils/semilleros.js
+++ src/utils/semilleros.js
@@ -49,21 +49,7 @@ const getSemillero = (semillero) => {
       logo: logistica,
       name: 'Siclog',
       description: `La línea de investigación está orientada la búsqueda de mejores prácticas logísticas para el desarrollo de cadenas de suministro en escenarios globalizados. Las nuevas expectativas de los mercados y las presiones tecnológicas del medio en el que se desarrollan las organizaciones, generan que los enfoques de la gestión estén reinventándose constantemente (García, 2006).
-      El modelo de cadena de suministros funciona como una red integrada por varios proveedores, fabricantes, distribuidores, y consumidores finales, coordinados armoniosamente para satisfacer los requerimientos del cliente, con el menor costo y tiempo posible (Whitman et al, 1998, Ballou, 2000). La complejidad de su gestión depende de múltiples factores y no puede desligarse del desarrollo de sistemas logísticos para lograr el flujo eficiente de materiales, información y capital entre los actores involucrados.
-      
-      Para gestionar la cadena de suministros se requiere conocer las necesidades propias sobre diseño de productos, abastecimiento, planificación y pronósticos, producción, distribución, y servicio postventa (Swaminathan y Tayur, 2003). Sin embargo, la dinámica de los mercados, los avances tecnológicos, los requerimientos legales, sociales y económicos, hacen que estas tareas sean cada vez más complejas de realizar, exigiendo un mayor grado de preparación, tecnificación y colaboración entre los actores involucrados.
-      
-      En el Contexto Colombiano, el desarrollo de algunas cadenas de suministros se muestra estático. Se adolece de herramientas para toma de decisiones oportunas, conocimiento para gestionar redes en escenarios internacionales, y la implementación de prácticas logísticas contemporáneas a costos razonables. De acuerdo con el estudio de caracterización del sector de la logística en Colombia (2014), en la gran mayoría de las empresas analizadas:
-      “se evidencia la ausencia de líderes transformacionales y por consiguiente el temor al cambio organizacional es la constante (...) Es necesario definir nuevas formas organizativas que incrementen los niveles de competitividad en integración de los Supply Chains y los niveles de competitividad en Logística (innovación). Los bajos niveles de integración en Supply Chains Management y la baja competitividad en Logística son otra causa de los altos costos logísticos” (Mesa sectorial de logística & SENA, 2014, 394.p)
-      
-      El estudio también revela una baja apropiación de estrategias logísticas para el desarrollo de cadenas de suministro dirigidas a afrontar los retos asociados al medio ambiente y al cambio climático. Las verdaderas macro tendencias se enmarcan en cómo afrontar, “el aumento de la población y la migración, la conectividad global (tecnología de información y las comunicaciones) y nivelación económica, (…)la nivelación de las expectativas de los clientes, la expansión del mercado (oferta/demanda) y la escasez de recursos” (Mesa sectorial de logística & SENA, 2014, 394.p)
-      
-      De acuerdo con lo anteriormente expuesto, la línea de investigación pretende abordar los retos contemporáneos para la Gestión de cadenas de suministro en contextos globalizados, a partir del desarrollo de las siguientes sublineas:
-      
-      - Industria 4.0 aplicada a logística y gestión de cadenas de suministro.
-      - Sostenibilidad en las cadenas de suministro.
-      - Comercio internacional.
-      - Transporte `,
+      El modelo de cadena de suministros funciona como una red integrada por varios proveedores, fabricantes, distribuidores, y consumidores finales, coordinados armoniosamente para satisfacer los requerimientos del cliente, con el menor costo y tiempo posible (Whitman et al, 1998, Ballou, 2000). La complejidad de su gestión depende de múltiples factores y no puede desligarse del desarrollo de sistemas logísticos para lograr el flujo eficiente de materiales, información y capital entre los actores involucrados.`,
       team: [
         {
           name: 'John Leuro',
