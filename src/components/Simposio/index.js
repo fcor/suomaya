003e203c6384a02bfd5230cb76e6ecba974cd0c4
Fabio Cortés <fcor14@hotmail.com>
2019-01-23 11:26:57 -0500

    Simposio gallery ok

--- src/components/Simposio/index.js
+++ src/components/Simposio/index.js
@@ -4,6 +4,12 @@ import flyer from '../../assets/img/simposio.jpg'
 import logoSimposio from '../../assets/img/simposio2.png'
 import sena from '../../assets/img/sena.png'
 import sennova from '../../assets/img/sennova.png'
+import foto1 from '../../assets/img/foto1.jpg'
+import foto2 from '../../assets/img/foto2.jpg'
+import foto3 from '../../assets/img/foto3.jpg'
+import foto4 from '../../assets/img/foto4.jpg'
+import foto5 from '../../assets/img/foto5.jpg'
+import foto6 from '../../assets/img/foto6.jpg'
 
 class Simposio extends React.Component {
   componentDidMount() {
@@ -37,8 +43,16 @@ class Simposio extends React.Component {
             src={flyer}
           />
         </section>
-        <section className="simposio-pics">
-
+        <section className="simposio-pics column">
+          <h1>¡Asi fue el II Simposio de Ciencia, innovación y tecnología!</h1>
+          <div className="simposio-gallery row">
+            <img src={foto1} alt="pic"/>
+            <img src={foto2} alt="pic"/>
+            <img src={foto3} alt="pic"/>
+            <img src={foto4} alt="pic"/>
+            <img src={foto5} alt="pic"/>
+            <img src={foto6} alt="pic"/>
+          </div>
         </section>
       </div>
     )
