ca9da74a144117c5ac6d3dca3313b0786a3395c7
Fabio Cortes <fcor14@hotmail.com>
2019-06-19 10:46:14 -0500

    Flyer updated

--- src/components/Simposio/index.js
+++ src/components/Simposio/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from '../Button'
 import './styles.css'
 import flyer from '../../assets/img/simposio.jpg'
+import flyer2 from '../../assets/img/simposio2.jpg'
 import logoSimposio from '../../assets/img/simposio2.png'
 import sena from '../../assets/img/sena.png'
 import sennova from '../../assets/img/sennova.png'
@@ -21,7 +22,7 @@ class Simposio extends React.Component {
       <div className="simposio">
         <section className="simposio-top row">
           <div className="simposio-top-info column">
-            <h1>II Simposio Ciencia, innovación y tecnología</h1>
+            <h1>III Simposio Ciencia, innovación y tecnología</h1>
             <p>
               Este evento surge ante la necesidad de difundir  y posicionar  
               los  procesos de  investigación, desarrollo tecnológico e innovación
@@ -34,17 +35,17 @@ class Simposio extends React.Component {
               <div className="support-logos row">
                 <img src={sena} alt="sena" width="90" />
                 <img src={sennova} alt="sennova" width="210" />
-                <img src={logoSimposio} alt="simposio" width="210" />
+                {/* <img src={logoSimposio} alt="simposio" width="210" /> */}
               </div>
               <Button link="https://docs.google.com/forms/d/e/1FAIpQLSeZa4JonFjq7s7oDPnDvMgRBAgRlPvMzPnNDFYcCo72-D0C6w/viewform">
-                  INSCRÍBETE AQUÍ!
+                  INSCRÍBETE AQUÍ
               </Button>
             </div>
           </div>
           <img 
             className="flyer"
             alt="flyer"
-            src={flyer}
+            src={flyer2}
           />
         </section>
         {/* <section className="simposio-pics column">
